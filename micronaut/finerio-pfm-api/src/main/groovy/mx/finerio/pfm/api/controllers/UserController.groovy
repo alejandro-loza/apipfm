96136ed6175b18956f021122d79dbc371c648c54
Alejandro <orochi_alex@hotmail.com>
2020-04-28 18:18:38 -0500

    added error handler

--- micronaut/finerio-pfm-api/src/main/groovy/mx/finerio/pfm/api/controllers/UserController.groovy
+++ micronaut/finerio-pfm-api/src/main/groovy/mx/finerio/pfm/api/controllers/UserController.groovy
@@ -1,8 +1,15 @@
 package mx.finerio.pfm.api.controllers
 
+import com.fasterxml.jackson.core.JsonParseException
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Error
+
+import io.micronaut.http.hateoas.JsonError
+import io.micronaut.http.hateoas.Link
+import io.micronaut.validation.Validated
 import io.reactivex.Single
 import mx.finerio.pfm.api.domain.User
 import mx.finerio.pfm.api.dtos.UserDto
@@ -10,8 +17,12 @@ import mx.finerio.pfm.api.pogos.UserCreateCommand
 import mx.finerio.pfm.api.services.UserService
 
 import javax.inject.Inject
+import javax.validation.ConstraintViolationException
 import javax.validation.Valid
+import io.micronaut.http.*
 
+
+@Validated
 @Controller("/users")
 class UserController {
     @Inject
@@ -22,4 +33,20 @@ class UserController {
         Single.just(new UserDto(userService.save(new User(cmd))))
     }
 
+    @Error
+    HttpResponse<JsonError> jsonError(HttpRequest request, ConstraintViolationException jsonParseException) {
+        JsonError error = new JsonError("Invalid JSON: " + jsonParseException.getMessage())
+                .link(Link.SELF, Link.of(request.uri))
+
+        HttpResponse.<JsonError> status(HttpStatus.BAD_REQUEST, "Fix Your JSON").body(error)
+    }
+
+    @Error(status = HttpStatus.BAD_REQUEST)
+    HttpResponse<JsonError> badRequest(HttpRequest request) {
+        JsonError error = new JsonError("Malformed User create request")
+                .link(Link.SELF, Link.of(request.getUri()))
+
+        HttpResponse.<JsonError>badRequest()
+                .body(error)
+    }
 }
