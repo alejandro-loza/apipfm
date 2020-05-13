c2373535d36a1af29c217687641fc19eafd51828
Alejandro <orochi_alex@hotmail.com>
2020-05-13 18:11:46 -0500

    added service to consult and get a financial entity

--- micronaut/finerio-pfm-api/src/main/groovy/mx/finerio/pfm/api/exceptions/FinancialEntityNotFoundException.groovy
+++ micronaut/finerio-pfm-api/src/main/groovy/mx/finerio/pfm/api/exceptions/FinancialEntityNotFoundException.groovy
@@ -1,7 +1,7 @@
 package mx.finerio.pfm.api.exceptions
 
 class FinancialEntityNotFoundException extends NotFoundException {
-    FinancialEntityNotFoundException(String message ) {
-        super( message )
+    FinancialEntityNotFoundException() {
+        super( 'financialEntity.exist' )
     }
 }
--- micronaut/finerio-pfm-api/src/test/groovy/mx/finerio/pfm/api/services/FinancialEntityServiceSpec.groovy
+++ micronaut/finerio-pfm-api/src/test/groovy/mx/finerio/pfm/api/services/FinancialEntityServiceSpec.groovy
@@ -1,6 +1,7 @@
 package mx.finerio.pfm.api.services
 
 import mx.finerio.pfm.api.domain.FinancialEntity
+import mx.finerio.pfm.api.exceptions.FinancialEntityNotFoundException
 import mx.finerio.pfm.api.services.gorm.FinancialEntityGormService
 import mx.finerio.pfm.api.services.imp.FinancialEntityServiceImp
 import mx.finerio.pfm.api.validation.FinancialEntityCommand
@@ -40,6 +41,28 @@ class FinancialEntityServiceSpec extends Specification {
         IllegalArgumentException e = thrown()
         e.message ==
                 'callbackService.create.createCallbackDto.null'
+    }
+
+    def "Should get an financial entity"(){
+
+        when:
+        1 * financialEntityService.financialEntityGormService.findByIdAndDateDeletedIsNull(_ as Long) >> new FinancialEntity()
+
+        def result = financialEntityService.getById(1L)
+
+        then:
+        result instanceof FinancialEntity
+    }
+
+    def "Should not get an financial entity and throw exception"(){
+
+        when:
+        1 * financialEntityService.financialEntityGormService.findByIdAndDateDeletedIsNull(_ as Long) >> null
+        financialEntityService.getById(666)
+
+        then:
+        FinancialEntityNotFoundException e = thrown()
+        e.message == 'financialEntity.exist'
     }
 
 }
