0b9e9069d225a9f815d09752ebfb102ed40de346
Alejandro <orochi_alex@hotmail.com>
2020-05-20 12:42:33 -0500

    added category gorm service

---
+++ micronaut/finerio-pfm-api/src/main/groovy/mx/finerio/pfm/api/services/gorm/CategoryGormService.groovy
@@ -0,0 +1,17 @@
+package mx.finerio.pfm.api.services.gorm
+
+import grails.gorm.services.Query
+import grails.gorm.services.Service
+import mx.finerio.pfm.api.domain.Category
+import mx.finerio.pfm.api.domain.Transaction
+
+@Service(Category)
+interface CategoryGormService {
+    Category save(Category category)
+    Category getById(Long id)
+    List<Category> findAllByDateDeletedIsNull(Map args)
+    List<Category> findAllByDateDeletedIsNullAndIdLessThanEquals(Long id, Map args)
+
+    @Query("from ${Category c} where $c.id = $id and c.dateDeleted is Null")
+    Transaction findByIdAndDateDeletedIsNull(Long id)
+}
