a2a1a45192eb30dac13d25795f25761aa2380b7c
Alejandro <orochi_alex@hotmail.com>
2020-06-04 12:26:02 -0500

    added tegister service and login test passes

--- micronaut/finerio-pfm-api/src/test/groovy/mx/finerio/pfm/api/controllers/LoginIntegrationSpec.groovy
+++ micronaut/finerio-pfm-api/src/test/groovy/mx/finerio/pfm/api/controllers/LoginIntegrationSpec.groovy
@@ -2,6 +2,7 @@ package mx.finerio.pfm.api.controllers
 
 import io.micronaut.context.annotation.Property
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.RxStreamingHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.exceptions.HttpClientResponseException
@@ -9,6 +10,7 @@ import io.micronaut.security.authentication.UsernamePasswordCredentials
 import io.micronaut.security.token.jwt.render.AccessRefreshToken
 import io.micronaut.test.annotation.MicronautTest
 import mx.finerio.pfm.api.Application
+import mx.finerio.pfm.api.services.RegisterService
 import mx.finerio.pfm.api.services.gorm.ClientGormService
 import mx.finerio.pfm.api.services.gorm.ClientRoleGormService
 import mx.finerio.pfm.api.services.gorm.RoleGormService
@@ -29,35 +31,22 @@ class LoginIntegrationSpec extends Specification {
     RxStreamingHttpClient client
 
     @Inject
-    ClientGormService clientGormService
-
-    @Inject
-    RoleGormService roleGormService
-
-    @Inject
-    ClientRoleGormService clientRoleGormService
-
-    @Inject
-    BCryptPasswordEncoderService passwordEncoderService
-
+    RegisterService registerService
 
     def "Should do login"(){
         given:
+        registerService.register( "sherlock", 'elementary', ['ROLE_DETECTIVE'])
 
-        UsernamePasswordCredentials creds = new UsernamePasswordCredentials("sherlock", "password")
-        final String encodedPassword = passwordEncoderService.encode(creds.password)
-
-        def client1 = clientGormService.save(creds.username, encodedPassword)
-        def role = roleGormService.save('ADMIN')
-        clientRoleGormService.save(client1, role)
-        HttpRequest request = HttpRequest.POST(LOGIN_ROOT, [username:creds.username, password:creds.password])
+        HttpRequest request = HttpRequest.POST(LOGIN_ROOT, [username:'sherlock', password:'elementary'])
 
         when:
         def response = client.toBlocking().exchange(request, AccessRefreshToken)
 
         then:
-        def  e = thrown HttpClientResponseException
-        e.response.body() == ''
-      //  response.status == HttpStatus.OK
+        response.status == HttpStatus.OK
+        response.body.get().accessToken
+        response.body.get().refreshToken
+        response.body().expiresIn
+
     }
 }
