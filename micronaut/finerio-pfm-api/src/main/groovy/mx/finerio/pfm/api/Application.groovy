474fe3cc5004cc7943a9dc32d671ab7c73fe4933
Alejandro <orochi_alex@hotmail.com>
2020-06-05 14:08:57 -0500

    deleted create client on startup

--- micronaut/finerio-pfm-api/src/main/groovy/mx/finerio/pfm/api/Application.groovy
+++ micronaut/finerio-pfm-api/src/main/groovy/mx/finerio/pfm/api/Application.groovy
@@ -1,29 +1,11 @@
 package mx.finerio.pfm.api
 
-import groovy.transform.CompileStatic
 import io.micronaut.runtime.Micronaut
-import mx.finerio.pfm.api.services.RegisterService
-
-import javax.inject.Singleton
-import io.micronaut.context.event.ApplicationEventListener
-import io.micronaut.runtime.server.event.ServerStartupEvent
+import groovy.transform.CompileStatic
 
 @CompileStatic
-@Singleton
-class Application implements ApplicationEventListener<ServerStartupEvent> {
-
-    protected final RegisterService registerService
-
-    Application(RegisterService registerService) {
-        this.registerService = registerService
-    }
-
-    @Override
-    void onApplicationEvent(ServerStartupEvent event) {
-        registerService.register("sherlock", 'elementary', ['ROLE_DETECTIVE'])
-    }
-
+class Application {
     static void main(String[] args) {
-        Micronaut.run(Application.class)
+        Micronaut.run(Application)
     }
 }
