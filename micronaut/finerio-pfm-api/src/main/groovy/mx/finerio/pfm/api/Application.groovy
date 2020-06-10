446dd273fb4bc780cb0979975bbdb8bb5da1c80a
Alejandro <orochi_alex@hotmail.com>
2020-06-10 16:56:00 -0500

    updated clonflict migration files

--- micronaut/finerio-pfm-api/src/main/groovy/mx/finerio/pfm/api/Application.groovy
+++ micronaut/finerio-pfm-api/src/main/groovy/mx/finerio/pfm/api/Application.groovy
@@ -1,11 +1,36 @@
 package mx.finerio.pfm.api
 
-import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
+import io.micronaut.runtime.Micronaut
+import mx.finerio.pfm.api.services.ClientService
+import mx.finerio.pfm.api.validation.ClientCommand
+
+import javax.inject.Singleton
+import io.micronaut.context.event.ApplicationEventListener
+import io.micronaut.runtime.server.event.ServerStartupEvent
 
 @CompileStatic
-class Application {
+@Singleton
+class Application implements ApplicationEventListener<ServerStartupEvent> {
+
+    protected final ClientService registerService
+
+    Application(ClientService registerService) {
+        this.registerService = registerService
+    }
+
+    @Override
+    void onApplicationEvent(ServerStartupEvent event) {
+        ClientCommand cmd = new ClientCommand()
+        cmd.with {
+            username = 'sherlock'
+            rawPassword = 'elementary'
+            authorities = ['ROLE_DETECTIVE']
+        }
+        registerService.register(cmd)
+    }
+
     static void main(String[] args) {
-        Micronaut.run(Application)
+        Micronaut.run(Application.class)
     }
 }
